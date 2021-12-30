5351b4c522c8c8da981b186b52363ae5b8080af8
Joni <artha699@gmail.com>
2021-12-30 17:41:07 +0800

    lte

---
+++ resources/views/home.blade.php
@@ -0,0 +1,26 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+
+
+
+@stop
+
+@section('content')
+
+Dashboard
+<br>
+
+Welcome to this beautiful admin panel.
+
+@stop
+
+@section('css')
+
+@stop
+
+@section('js')
+
+@stop

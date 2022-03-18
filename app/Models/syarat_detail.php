99e64722cde4ab798cbfce7a455f40c1d41ee89f
Joni <artha699@gmail.com>
2022-03-18 13:10:23 +0800

    controller dan model

---
+++ app/Models/syarat_detail.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class syarat_detail extends Model
+{
+    use HasFactory;
+}
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,12 @@
 <?php
 
 use App\Http\Controllers\barangController;
+use App\Http\Controllers\jenis_kontrakController;
+use App\Http\Controllers\jenis_pengadaanController;
 use App\Http\Controllers\katagori_barangController;
 use App\Http\Controllers\komentarController;
+use App\Http\Controllers\metode_pengadaanController;
+use App\Http\Controllers\StatusTenderController;
 use App\Models\katagori_barang;
 use Illuminate\Support\Facades\Route;
 
@@ -31,5 +35,12 @@ Route::middleware(['middleware' => 'auth' ])->group(function () {
     Route::post('/photoStore',[barangController::class,'photoStore'])->name('photo.simpan');
     Route::resource('/katagori', katagori_barangController::class);
     Route::resource('komentar',komentarController::class);
+
+    //resource
+    Route::resource('jenis_pengadaan',jenis_pengadaanController::class);
+    Route::resource('jenis_kontrak',jenis_kontrakController::class);
+    Route::resource('metode_pengadaan',metode_pengadaanController::class);
+    Route::resource('status_tender',StatusTenderController::class);
+    
 });
 

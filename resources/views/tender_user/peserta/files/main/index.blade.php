7eefbb7e71f6cd39e67b89ebe43d4ea43ce24048
joni <artha699@gmail.com>
2022-10-28 00:40:00 +0800

    edit sedikit

--- resources/views/tender_user/peserta/files/main/index.blade.php
+++ resources/views/tender_user/peserta/files/main/index.blade.php
@@ -2,35 +2,38 @@
     <div class="card">
       <div class="card-header p-2">
         <ul class="nav nav-pills text-center">
-          <li class="nav-item"><a class="nav-link active" href="#activity" data-toggle="tab">Administrasi</a></li>
-          <li class="nav-item"><a class="nav-link" href="#timeline" data-toggle="tab">Persyaratan Kualifikasi</a></li>
+          <li class="nav-item"><a class="nav-link active" href="#activity" data-toggle="tab">Persyaratan Kualifikasi</a></li>
+          <li class="nav-item"><a class="nav-link" href="#timeline" data-toggle="tab">Administrasi</a></li>
           <li class="nav-item"><a class="nav-link" href="#settings" data-toggle="tab">Evaluasi Teknis</a></li>
-          <li class="nav-item"><a class="nav-link" href="#s1" data-toggle="tab">Penawaran Peserta</a></li>
+          <li class="nav-item"><a class="nav-link" href="#s1" data-toggle="tab">Harga</a></li>
         </ul>
       </div><!-- /.card-header -->
       <div class="card-body">
         <div class="tab-content">
           <div class="active tab-pane" id="activity">
             <!-- Aktivity -->
-            <h1 class="text-center">
-                Administrasi
-                @include('tender_user.peserta.admin.file3')
-                @include('tender_user.peserta.files.penilaian.admin')
-            </h1>
+                <h2 class="text-center">Persyaratan Kualifikasi</h2>
+                    @include('tender_user.peserta.admin.file')
+
+                    <h3 class="text-center">Pengalaman</h3>
+                    @include('tender_user.peserta.admin.pengalaman')
+                    <h3 class="text-center">Pekerjaan Sedang Berjalan</h3>
+                    @include('tender_user.peserta.admin.pekerjaan_berjalan')
+                    {{-- Penilaian --}}
+                    @include('tender_user.peserta.files.penilaian.kualifikasi')
             <!-- End Aktivity -->
           </div>
           <!-- /.tab-pane -->
           <div class="tab-pane" id="timeline">
             <!-- The timeline -->
-            <h2 class="text-center">Persyaratan Kualifikasi</h2>
-                @include('tender_user.peserta.admin.file')
+            <h1 class="text-center">
+                Administrasi
 
-                <h3 class="text-center">Pengalaman</h3>
-                @include('tender_user.peserta.admin.pengalaman')
-                <h3 class="text-center">Pekerjaan Sedang Berjalan</h3>
-                @include('tender_user.peserta.admin.pekerjaan_berjalan')
-                {{-- Penilaian --}}
-                @include('tender_user.peserta.files.penilaian.kualifikasi')
+            </h1>
+                @include('tender_user.peserta.admin.file3')
+                @include('tender_user.peserta.files.penilaian.admin')
+
+
 
 
           </div>
